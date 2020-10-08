dda71dafd6361e621fe21fc6bf38cc376a629fa7
Annet-Bon <66079586+Annet-Bon@users.noreply.github.com>
2020-10-08 22:35:03 +0300

    try3.0

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,22 @@
+// const inputRef = document.querySelector('#validation-input');
+
+// const borderPainter = event => {
+//   if (event.currentTarget.value.length === Number(inputRef.getAttribute('data-length'))) {
+//     inputRef.setAttribute('class', 'valid');
+//   } else inputRef.setAttribute('class', 'invalid');
+// };
+
+// inputRef.addEventListener('blur', borderPainter);
+
 const inputRef = document.querySelector('#validation-input');
 
 const borderPainter = event => {
   if (event.currentTarget.value.length === Number(inputRef.getAttribute('data-length'))) {
-    inputRef.setAttribute('class', 'valid');
-  } else inputRef.setAttribute('class', 'invalid');
-};
+    inputRef.classList.remove('invalid')
+    inputRef.classList.add('valid');
+  } else {
+    inputRef.classList.remove('valid')
+    inputRef.classList.add('invalid');
+  }};
 
 inputRef.addEventListener('blur', borderPainter);
